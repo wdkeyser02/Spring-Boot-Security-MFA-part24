5076640bd13964496318d1852511461474757fd6
Willy De Keyser <spring.boot.programming@gmail.com>
2026-05-12 11:09:14 +0200

    Init

---
+++ Spring-Boot-Security-MFA-part24/src/main/java/willydekeyser/SpringBootSecurityMfaPart24Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityMfaPart24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityMfaPart24Application.class, args);
+	}
+
+}
